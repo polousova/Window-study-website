a111917709b75c95313955c0c6d8c632d5769e3d
Dina Polousova <dinapolousova@gmail.com>
2023-05-30 09:53:39 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,43 @@
 "use strict";
 
+//импорт пакетов для проекта
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
+//путь, куда компилируется 
 const dist = "./dist/";
 
+//задачи, которые собирают проект
+
+//отслеживание изменений в html
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
+                .pipe(gulp.dest(dist))//переместить файл в папку dist
+                .pipe(browsersync.stream());//запускаем автообновление страницы через browsersync
 });
 
+//компиляция скриптовых файлов
 gulp.task("build-js", () => {
-    return gulp.src("./src/js/main.js")
+    return gulp.src("./src/js/main.js")//запускаем webpack на основном файле скрипта main.js
                 .pipe(webpack({
-                    mode: 'development',
+                    mode: 'development',//режим разработки webpack
                     output: {
-                        filename: 'script.js'
+                        filename: 'script.js'//указываем куда будет помещаться output файл
                     },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
+                    watch: false, //за watch отвечает отдельная задача, поэтому здесь отключаем
+                    devtool: "source-map",//создаем карту проекта
+                    module: {//подключаемые модули
                         rules: [
                           {
                             test: /\.m?js$/,
                             exclude: /(node_modules|bower_components)/,
                             use: {
-                              loader: 'babel-loader',
+                              loader: 'babel-loader',//модуль babel
                               options: {
-                                presets: [['@babel/preset-env', {
+                                presets: [['@babel/preset-env', {//с пресетом env
                                     debug: true,
-                                    corejs: 3,
+                                    corejs: 3,//3я версия Core JS
                                     useBuiltIns: "usage"
                                 }]]
                               }
@@ -40,16 +46,18 @@ gulp.task("build-js", () => {
                         ]
                       }
                 }))
-                .pipe(gulp.dest(dist))
-                .on("end", browsersync.reload);
+                .pipe(gulp.dest(dist)) //полученный файл помещается в папку dist
+                .on("end", browsersync.reload); //если были изменения в файле main.js, страница перезагружается
 });
 
+//если что-то изменяется в папке src/assets, копируем все файлы из папки assets в папку dist/assets и перезагружаем страницу
 gulp.task("copy-assets", () => {
     return gulp.src("./src/assets/**/*.*")
                 .pipe(gulp.dest(dist + "/assets"))
                 .on("end", browsersync.reload);
 });
 
+//задача watch - запускает browsersync на порте 4000
 gulp.task("watch", () => {
     browsersync.init({
 		server: "./dist/",
@@ -57,13 +65,16 @@ gulp.task("watch", () => {
 		notify: true
     });
     
+    //следим за изменениями отдельных файлов
     gulp.watch("./src/index.html", gulp.parallel("copy-html"));
     gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
     gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
 });
 
+//запуск билда
 gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js"));
 
+//запускаем webpack на main.js в режиме production
 gulp.task("build-prod-js", () => {
     return gulp.src("./src/js/main.js")
                 .pipe(webpack({
@@ -92,4 +103,5 @@ gulp.task("build-prod-js", () => {
                 .pipe(gulp.dest(dist));
 });
 
+//запуск по умолчанию build и watch
 gulp.task("default", gulp.parallel("watch", "build"));
